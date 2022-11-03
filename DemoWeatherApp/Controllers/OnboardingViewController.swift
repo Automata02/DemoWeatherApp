dc06e45b9390bba15e1b1e0dfc4419629a6c30fc
Roberts Kursitis <53969619+Automata02@users.noreply.github.com>
2022-11-03 16:09:05 +0200

    Onboarding UI improvements
    Fixed issues with the view breaking on smaller and larger devices by adjusting the constraints of subviews.

--- DemoWeatherApp/Controllers/OnboardingViewController.swift
+++ DemoWeatherApp/Controllers/OnboardingViewController.swift
@@ -4,6 +4,7 @@
 //
 //  Created by roberts.kursitis on 02/11/2022.
 //
+#warning("TODO: Update second view's button after location services are enabled.")
 
 import UIKit
 import CoreLocation
@@ -14,6 +15,11 @@ class OnboardingViewController: UIViewController {
     
     let scrollView = UIScrollView()
     let locationManager = CLLocationManager()
+    
+    let TitleLabel = UILabel()
+    let ImageView = UIImageView()
+    let TextLabel = UILabel()
+    let Button = UIButton()
     
     var isStyled: Bool = false
 
@@ -27,87 +33,161 @@ class OnboardingViewController: UIViewController {
     }
     
     private func configure() {
-
-        scrollView.frame = holderView.bounds
-        holderView.addSubview(scrollView)
-                
         let titles = [
             "Welcome!",
             "Location!",
-            "Almost Ready!"
+            "Location!",
+            "Done!"
         ]
         
-        for page in 0...2 {
+        scrollView.frame = holderView.bounds
+        holderView.addSubview(scrollView)
+        
+        for page in 0..<4 {
             let pageView = UIView(frame: CGRect(x: CGFloat(page) * holderView.frame.size.width, y: 0, width: holderView.frame.size.width, height: holderView.frame.size.height))
             scrollView.addSubview(pageView)
             
-            let obTitleLabel = UILabel(frame: CGRect(x: 10, y: 10, width: pageView.frame.size.width - 20, height: 120))
-            let obImageView = UIImageView(frame: CGRect(x: 60, y: 50, width: pageView.frame.size.width - 120, height: pageView.frame.height - 60 - 130 - 15))
-            let obTextLabel = UILabel(frame: CGRect(x: 30, y: 80, width: pageView.frame.size.width - 60, height: 100))
-            let obButton = UIButton(frame: CGRect(x: 50, y: pageView.frame.size.height - 100, width: pageView.frame.size.width - 100, height: 50))
+            let obTitleLabel = UILabel()
+            let obImageView = UIImageView()
+            let obTextLabel = UILabel()
+            let obButton = UIButton()
+            
+            obTitleLabel.translatesAutoresizingMaskIntoConstraints = false
+            obImageView.translatesAutoresizingMaskIntoConstraints = false
+            obTextLabel.translatesAutoresizingMaskIntoConstraints = false
+            obButton.translatesAutoresizingMaskIntoConstraints = false
             
             obTitleLabel.textAlignment = .center
-            obTitleLabel.font = .systemFont(ofSize: 32, weight: .semibold)
+            obTitleLabel.font = .systemFont(ofSize: 31, weight: .semibold)
             pageView.addSubview(obTitleLabel)
             obTitleLabel.text = titles[page]
             
             obImageView.contentMode = .scaleAspectFit
             pageView.addSubview(obImageView)
             
-            obTextLabel.font = .systemFont(ofSize: 22, weight: .medium)
+            obTextLabel.font = .systemFont(ofSize: 21, weight: .regular)
             obTextLabel.numberOfLines = 0
-            obTextLabel.lineBreakMode = .byWordWrapping
+            obTextLabel.lineBreakMode = .byClipping
             obTextLabel.textAlignment = .justified
             pageView.addSubview(obTextLabel)
-
 
             var config = UIButton.Configuration.filled()
             config.baseBackgroundColor = .systemGreen
             config.baseForegroundColor = .white
             config.imagePlacement = .leading
             config.imagePadding = 5
-            
+            config.buttonSize = .large
             
             switch page {
             case 0:
                 obImageView.image = UIImage(named: "NordicWeather")
                 obButton.setTitle("Continue", for: .normal)
-                obTextLabel.text = "To use Nordic Weather app we need to do some setup"
+                obTextLabel.text = "To fully use Nordic Weather app we need to briefly set up a few things."
             case 1:
                 obImageView.image = UIImage(systemName: "mappin.and.ellipse")
                 obButton.setTitle("Set location permission", for: .normal)
-                obTextLabel.text = "We'll provide weather forecast for your location."
+                obTextLabel.text = "We are excited to provide you with weather forecast for your current location. However to present accurate data we need to have Location Services enabled. The app does not store this nor any personal data outside of your iOS device."
                 config.image = UIImage(systemName: "location.fill")
                 config.baseBackgroundColor = .systemBlue
                 config.baseForegroundColor = .white
             case 2:
+                obImageView.image = UIImage(systemName: "mappin.and.ellipse")
+                obImageView.isHidden = true
+                obButton.setTitle("Continue", for: .normal)
+                obTextLabel.text = "We're almost there!"
+                obTextLabel.textAlignment = .center
+                config.image = UIImage(systemName: "location.fill")
+                config.baseBackgroundColor = .systemBlue
+                config.baseForegroundColor = .white
+            case 3:
                 obImageView.image = UIImage(systemName: "clock.badge")
+                obTextLabel.text = "All done, daily forecast notifications can be enabled within settings as well as Nordic Weather app's theme can be adjusted as from the settings."
                 obButton.setTitle("Continue", for: .normal)
             default:
                 return
             }
-            obButton.configuration = config
             
+            obButton.configuration = config
             obButton.tag = page + 1
             obButton.addTarget(self, action: #selector(didTapButton), for: .touchUpInside)
             pageView.addSubview(obButton)
-            styleView(views: [obButton, obImageView])
+            
+            //MARK: Constraints
+            NSLayoutConstraint.activate([
+                obTitleLabel.heightAnchor.constraint(equalToConstant: 50),
+                obTitleLabel.centerXAnchor.constraint(equalTo: pageView.centerXAnchor),
+                obTitleLabel.centerYAnchor.constraint(equalTo: pageView.topAnchor, constant: 60),
+                
+                obImageView.widthAnchor.constraint(equalToConstant: pageView.frame.width - 60),
+                obImageView.heightAnchor.constraint(equalToConstant: pageView.frame.width - 60),
+                obImageView.centerXAnchor.constraint(equalTo: pageView.centerXAnchor),
+                obImageView.centerYAnchor.constraint(equalTo: obTitleLabel.bottomAnchor, constant: 180),
+                
+                obTextLabel.widthAnchor.constraint(equalToConstant: pageView.frame.width - 60),
+                obTextLabel.topAnchor.constraint(equalTo: obImageView.bottomAnchor, constant: 20),
+                obTextLabel.centerXAnchor.constraint(equalTo: pageView.centerXAnchor),
+                
+                obButton.centerXAnchor.constraint(equalTo: pageView.centerXAnchor),
+                obButton.bottomAnchor.constraint(equalTo: pageView.bottomAnchor, constant: -30)
+            ])
         }
-        scrollView.contentSize = CGSize(width: holderView.frame.size.width * 3, height: 0)
+        scrollView.contentSize = CGSize(width: holderView.frame.size.width * 4, height: 0)
         scrollView.isPagingEnabled = true
         scrollView.showsHorizontalScrollIndicator = false
         scrollView.isScrollEnabled = false
     }
     
+    func slidePage(button: UIButton) {
+        scrollView.setContentOffset(CGPoint(x: holderView.frame.size.width * CGFloat(button.tag), y: 0), animated: true)
+    }
+    
     @objc func didTapButton(_ button: UIButton) {
         if button.tag == 2 {
             locationManager.requestAlwaysAuthorization()
+            locationManagerDidChangeAuthorization(locationManager, button: button)
+            //MARK: return here prevent views from switching on first press
+//            return
         }
-        guard button.tag < 3 else {
+        guard button.tag < 4 else {
             NewUserCheck.shared.notNewUser()
             dismiss(animated: true)
             return
         }
-        scrollView.setContentOffset(CGPoint(x: holderView.frame.size.width * CGFloat(button.tag), y: 0), animated: true)
+        slidePage(button: button)
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager, button: UIButton) {
+        let status = manager.authorizationStatus
+
+        switch status {
+        case .denied, .notDetermined, .restricted:
+            DispatchQueue.main.asyncAfter(deadline: .now()){
+                self.locationManager.requestAlwaysAuthorization()
+            }
+        case .authorizedWhenInUse:
+            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                self.actionSheet(title: "Heads Up!", message: "Thank you for enabling location services, whilst they will allows the app to function some features might not work correctly as they're enabled by allowing the app to use location services While in Use.")
+            }
+            slidePage(button: button)
+        case .authorizedAlways:
+            print("filler")
+            slidePage(button: button)
+        default:
+            return
+        }
     }
 }
+
+extension OnboardingViewController {
+    private func actionSheet(title: String?, message: String?) {
+        DispatchQueue.main.async {
+            let ac = UIAlertController(title: title, message: message, preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "Ok", style: .default))
+            
+            self.present(ac, animated: true)
+        }
+    }
+}
+
+
+
